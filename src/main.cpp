8ae05ef68e935372e5ea91125d100c222f78b90a
samarqureshii <samarq713@gmail.com>
2023-05-24 11:51:32 -0400

    added adafruit library

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,20 @@
 #include <Arduino.h>
+int led1 = 6;
 
 // put function declarations here:
-int myFunction(int, int);
+
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(led1, OUTPUT);
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(led1, HIGH);
+  delay(1000);
+  digitalWrite(led1, LOW);
+  delay(1000);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
